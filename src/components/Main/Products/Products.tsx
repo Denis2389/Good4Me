40697ead220f81cb418b76830473940d75d785cc
Denis2389 <bereznyakdenis2003@gmail.com>
2025-01-24 10:22:20 +0200

    saved

--- src/components/Main/Products/Products.tsx
+++ src/components/Main/Products/Products.tsx
@@ -1,12 +1,12 @@
+import 'swiper/swiper-bundle.css'
 import styles from './Products.module.scss'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import '../../../../node_modules/swiper/swiper-bundle.css'
 import { Navigation } from 'swiper/modules';
 import product1 from '/images/Main/Products/product1.png';
 import product2 from '/images/Main/Products/product2.png';
 import product3 from '/images/Main/Products/product3.png'
 import girlPng from './girl.png'
-import './button.css'
+
 
 const Products = () => {
     return (
@@ -22,13 +22,13 @@ const Products = () => {
             Vegan friendly It is a long established fact that a reader will be
             distracted by the...
           </p>
-          <div className={styles.swiperCont}>
+          <div>
             <Swiper
-              spaceBetween={30}
+              spaceBetween={3}
               loop={true}
               slidesPerView={3}
               navigation={true}
-              className={styles.swiperBlock}
+              className={styles.firstSwiper}
               modules={[Navigation]}
               breakpoints={{
                 320: { slidesPerView: 1 },
@@ -36,7 +36,7 @@ const Products = () => {
                 1024: { slidesPerView: 3 },
               }}
             >
-              <SwiperSlide className={styles.swiperSlide}>
+              <SwiperSlide className={styles.firstSwiperSlide}>
                 <div className={styles.cardList}>
                   <img src={product1} alt="products1" />
                   <h5>FULL RANGE VALUE PACK</h5>
@@ -45,7 +45,7 @@ const Products = () => {
                   </strong>
                 </div>
               </SwiperSlide>
-              <SwiperSlide className={styles.swiperSlide}>
+              <SwiperSlide className={styles.firstSwiperSlide}>
                 <div className={styles.cardList}>
                   <img src={product2} alt="products2" />
                   <h5>FULL RANGE VALUE PACK </h5>
@@ -54,7 +54,7 @@ const Products = () => {
                   </strong>
                 </div>
               </SwiperSlide>
-              <SwiperSlide className={styles.swiperSlide}>
+              <SwiperSlide className={styles.firstSwiperSlide}>
                 <div className={styles.cardList}>
                   <img src={product3} alt="products3" />
                   <h5>FULL RANGE VALUE PACK </h5>
@@ -63,7 +63,7 @@ const Products = () => {
                   </strong>
                 </div>
               </SwiperSlide>
-              <SwiperSlide className={styles.swiperSlide}>
+              <SwiperSlide className={styles.firstSwiperSlide}>
                 <div className={styles.cardList}>
                   <img src={product2} alt="products2" />
                   <h5>FULL RANGE VALUE PACK </h5>
@@ -72,7 +72,7 @@ const Products = () => {
                   </strong>
                 </div>
               </SwiperSlide>
-              <SwiperSlide className={styles.swiperSlide}>
+              <SwiperSlide className={styles.firstSwiperSlide}>
                 <div className={styles.cardList}>
                   <img src={product1} alt="products1" />
                   <h5>FULL RANGE VALUE PACK</h5>
@@ -81,7 +81,7 @@ const Products = () => {
                   </strong>
                 </div>
               </SwiperSlide>
-              <SwiperSlide className={styles.swiperSlide}>
+              <SwiperSlide className={styles.firstSwiperSlide}>
                 <div className={styles.cardList}>
                   <img src={product2} alt="products2" />
                   <h5>FULL RANGE VALUE PACK </h5>
